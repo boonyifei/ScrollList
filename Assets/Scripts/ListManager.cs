ab4477f733e334766d5fba2b4828bcfba90272ed
Boon Yi Fei <boonyifei@gmail.com>
2018-01-17 11:33:33 +0800

    Bug Fix
    Bug fix: number of buffer was wrongly dependent on number of visible items
    Bug fix: size of elements was wrongly read from sizeDelta. It should read from rect.size instead

--- Assets/Scripts/ListManager.cs
+++ Assets/Scripts/ListManager.cs
@@ -8,7 +8,8 @@ public enum LISTDIRECTION
     VERTICAL = 1
 }
 
-public class ListManager : MonoBehaviour {
+public class ListManager : MonoBehaviour
+{
 
     public RectTransform Container;
     public Mask Mask;
@@ -20,7 +21,7 @@ public class ListManager : MonoBehaviour {
 
     private RectTransform maskRT;
     private int numVisible;
-    private int numVisibleHalf;
+    private int numBuffer = 2;
     private float containerHalfSize;
     private float prefabSize;
 
@@ -32,56 +33,56 @@ public class ListManager : MonoBehaviour {
     private Vector3 offsetVec;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
+        Container.anchoredPosition3D = new Vector3(0, 0, 0);
+
         maskRT = Mask.GetComponent<RectTransform>();
-   
-        Vector2 prefabScale = Prefab.sizeDelta;
-        prefabSize = (Direction == LISTDIRECTION.HORIZONTAL ? prefabScale.x: prefabScale.y) + Spacing;
+
+        Vector2 prefabScale = Prefab.rect.size;
+        prefabSize = (Direction == LISTDIRECTION.HORIZONTAL ? prefabScale.x : prefabScale.y) + Spacing;
 
         Container.sizeDelta = Direction == LISTDIRECTION.HORIZONTAL ? (new Vector2(prefabSize * Num, prefabScale.y)) : (new Vector2(prefabScale.x, prefabSize * Num));
-        containerHalfSize = Direction == LISTDIRECTION.HORIZONTAL ? (Container.sizeDelta.x * 0.5f):(Container.sizeDelta.y * 0.5f);
-  
-        numVisible = Mathf.CeilToInt((Direction == LISTDIRECTION.HORIZONTAL ? maskRT.sizeDelta.x: maskRT.sizeDelta.y) / prefabSize);
-        numVisibleHalf = Mathf.CeilToInt(numVisible * 0.5f);
+        containerHalfSize = Direction == LISTDIRECTION.HORIZONTAL ? (Container.rect.size.x * 0.5f) : (Container.rect.size.y * 0.5f);
+
+        numVisible = Mathf.CeilToInt((Direction == LISTDIRECTION.HORIZONTAL ? maskRT.rect.size.x : maskRT.rect.size.y) / prefabSize);
 
         offsetVec = Direction == LISTDIRECTION.HORIZONTAL ? Vector3.right : Vector3.down;
-        startPos = Container.anchoredPosition3D - (offsetVec * containerHalfSize) + (offsetVec * ((Direction == LISTDIRECTION.HORIZONTAL?prefabScale.x : prefabScale.y) * 0.5f));
-        numItems = Optimize? Mathf.Min(Num, numVisible + 2) : Num;
-        for (int i=0; i< numItems; i++)
+        startPos = Container.anchoredPosition3D - (offsetVec * containerHalfSize) + (offsetVec * ((Direction == LISTDIRECTION.HORIZONTAL ? prefabScale.x : prefabScale.y) * 0.5f));
+        numItems = Optimize ? Mathf.Min(Num, numVisible + numBuffer) : Num;
+        for (int i = 0; i < numItems; i++)
         {
-            GameObject obj = Instantiate(Prefab.gameObject, Container.transform);
+            GameObject obj = (GameObject)Instantiate(Prefab.gameObject, Container.transform);
             RectTransform t = obj.GetComponent<RectTransform>();
             t.anchoredPosition3D = startPos + (offsetVec * i * prefabSize);
             listItemRect.Add(t);
-            itemDict.Add(t.GetInstanceID(), new int[] {i,i});
+            itemDict.Add(t.GetInstanceID(), new int[] { i, i });
             obj.SetActive(true);
 
             ListItem li = obj.GetComponentInChildren<ListItem>();
             listItems.Add(li);
             li.UpdateContent(i);
         }
-        Container.anchoredPosition3D += offsetVec * (containerHalfSize - ((Direction == LISTDIRECTION.HORIZONTAL ? maskRT.sizeDelta.x : maskRT.sizeDelta.y) * 0.5f));
+        Prefab.gameObject.SetActive(false);
+        Container.anchoredPosition3D += offsetVec * (containerHalfSize - ((Direction == LISTDIRECTION.HORIZONTAL ? maskRT.rect.size.x : maskRT.rect.size.y) * 0.5f));
     }
 
     public void ReorderItemsByPos(float normPos)
     {
         if (Direction == LISTDIRECTION.VERTICAL) normPos = 1f - normPos;
-        int numOutOfView = Mathf.CeilToInt(normPos * (Num - numVisible));
-        int firstIndex = Mathf.Max(0, numOutOfView - numVisibleHalf);
+        int numOutOfView = Mathf.CeilToInt(normPos * (Num - numVisible));   //number of elements beyond the left boundary (or top)
+        int firstIndex = Mathf.Max(0, numOutOfView - numBuffer);   //index of first element beyond the left boundary (or top)
         int originalIndex = firstIndex % numItems;
 
         int newIndex = firstIndex;
-        for (int i=originalIndex; i< numItems; i++)
+        for (int i = originalIndex; i < numItems; i++)
         {
-            RectTransform item = listItemRect[i];
             moveItemByIndex(listItemRect[i], newIndex);
             listItems[i].UpdateContent(newIndex);
             newIndex++;
         }
         for (int i = 0; i < originalIndex; i++)
         {
-            RectTransform item = listItemRect[i];
             moveItemByIndex(listItemRect[i], newIndex);
             listItems[i].UpdateContent(newIndex);
             newIndex++;
